257a0b65a29490da371e2d4a9e70e2aa05639bba
Manuel Reinhardt <manuel.rhdt@gmail.com>
2018-01-05 19:28:46 +0100

    Update Example

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,12 +1,29 @@
 extern crate harfbuzz_rs;
 
 use harfbuzz_rs::{Face, Font, UnicodeBuffer};
+use harfbuzz_rs::rusttype::font_set_rusttype_funcs;
 
+// Execute this file from the root directory of this repository.
+//
+// The output should look like the following:
+// gid09=00@ 634,0+0
+// gid32=01@ 478,0+0
+// gid39=02@ 230,0+0
+// gid39=03@ 230,0+0
+// gid42=04@ 520,0+0
+// gid01=05@ 200,0+0
+// gid24=06@ 764,0+0
+// gid42=07@ 532,0+0
+// gid45=08@ 306,0+0
+// gid39=09@ 230,0+0
+// gid31=10@ 540,0+0
+// gid1146=11@ 248,0+0
 fn main() {
     let index = 0;
-    let path = "testfiles/Optima.ttc";
+    let path = "testfiles/SourceSansVariable-Roman.ttf";
     let face = Face::from_file(path, index).expect("Error reading font file.");
     let mut font = Font::new(face);
+    font_set_rusttype_funcs(&mut font);
 
     font.set_scale(20 * 64, 20 * 64);
     font.set_ppem(72 * 64, 72 * 64);
